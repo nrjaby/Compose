62427f697a2520223c35b4b996c0d18086dad446
nirajsingh <niraj.semi@gmail.com>
2024-12-08 16:45:11 +0530

    Add CompositionLocal example

---
+++ app/src/main/java/com/niraj/compose/compositionlocal/AppRoot.kt
@@ -0,0 +1,21 @@
+package com.niraj.compose.compositionlocal
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun AppRoot( ) {
+
+    // Mock data
+    val currentUser = remember { User(id = "12345", name = "John Doe") }
+    val appTheme = remember { AppTheme(primaryColor = Color(0xFF6200EA), secondaryColor = Color(0xFF03DAC5)) }
+
+    CompositionLocalProvider(
+        LocalUser provides currentUser,
+        LocalTheme provides appTheme
+    ) {
+        AppScreen()
+    }
+}
